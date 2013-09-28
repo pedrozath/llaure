0de22ff8a6e503e7697fa472936ba92d983f302d
Pedro Maciel <pedrozath@gmail.com>
2013-09-28 16:39:42 -0300

    updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
 	def index
 		@content_class = "full"
 		@logo = "logo-cia-das-capas-negativo.png"
-		@photos = Photo.all.shuffle.slice(0...3)
+		@photos = Photo.find(1)
+		@photos << Photo.find(2)
+		@photos << Photo.find(3)
 	end
 end
