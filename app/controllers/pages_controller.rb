0132cb2553df3fca7292c34ab52106012278b97b
Pedro Maciel <pedrozath@gmail.com>
2013-10-02 21:49:37 -0300

    subindo no heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
 	def index
 		@content_class = "full"
 		@logo = "logo-cia-das-capas-negativo.png"
-		@photos = ENV["FRONT_PHOTOS_IDS"].split(";").collect(&:to_i)
+		@photos = Photo.find ENV["FRONT_PHOTOS_IDS"].split(";").collect(&:to_i)
 	end
 end
