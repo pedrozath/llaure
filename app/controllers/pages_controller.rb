77dd12568c82a46bd723837586bc7dead1487d4b
Pedro Maciel <pedrozath@gmail.com>
2013-10-02 21:46:20 -0300

    front photos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
 	def index
 		@content_class = "full"
 		@logo = "logo-cia-das-capas-negativo.png"
-		@photos = ENV["FRONT_PHOTOS_IDS"].split(";")
+		@photos = ENV["FRONT_PHOTOS_IDS"].split(";").collect(&:to_i)
 	end
 end
