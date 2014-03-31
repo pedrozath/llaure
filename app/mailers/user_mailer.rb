2dfbe9448e6687e35fbcc09a7d19e92f8f8d0925
Pedro Maciel <pedrozath@gmail.com>
2014-03-31 06:58:28 -0300

    subindo no heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,10 @@ class UserMailer < ActionMailer::Base
   default :from => "#{ENV['EMAIL_LOGIN']}@#{ENV['EMAIL_DOMAIN']}"
   def contact_mail(contact)
   	@contact = contact
-    attachments[contact[:attachment].original_filename] = File.read(contact[:attachment].tempfile)
+
+    unless contact[:attachment].nil?
+    	attachments[contact[:attachment].original_filename] = File.read(contact[:attachment].tempfile)
+    end
 
   	mail to: ENV['EMAIL_RECEIVER'],
   		 bcc: 'pedrozath@gmail.com',
