9388172e650d3e37a4b66fde6049bd4fd8765bca
Pedro Maciel <pedrozath@gmail.com>
2016-06-06 19:40:23 -0300

    changing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 	def index
 		@content_class = "full"
-		@logo = "logo-cia-das-capas-negativo.png"
+		@logo = "llaure-logo-branco.png"
 		@photos = Photo.find ENV["FRONT_PHOTOS_IDS"].split(";").collect(&:to_i)
 	end
 end
